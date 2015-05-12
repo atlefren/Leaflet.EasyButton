0cdb741a8b124b24717f97c2f1b1c3b6b1a63570
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-05-12 13:56:38 +0200

    refactor

--- easy-button.js
+++ easy-button.js
@@ -1,15 +1,25 @@
+/*global L:false */
+
+'use strict';
+
 L.Control.EasyButtons = L.Control.extend({
+
     options: {
         position: 'topleft',
         title: '',
-        intendedIcon: 'fa-circle-o'
+        icon: 'fa-circle-o'
+    },
+
+    initialize: function (callback, options) {
+        L.setOptions(this, options);
+        this.callback = callback;
     },
 
     onAdd: function () {
         var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
 
         this.link = L.DomUtil.create('a', 'leaflet-bar-part', container);
-        this._addImage()
+        this._addImage();
         this.link.href = '#';
 
         L.DomEvent.on(this.link, 'click', this._click, this);
@@ -18,40 +28,29 @@ L.Control.EasyButtons = L.Control.extend({
         return container;
     },
 
-    intendedFunction: function(){ alert('no function selected');},
-
     _click: function (e) {
         L.DomEvent.stopPropagation(e);
         L.DomEvent.preventDefault(e);
-        this.intendedFunction();
+        if (typeof this.callback === 'function') {
+            this.callback();
+        } else {
+            throw new Error('no function selected');
+        }
     },
 
     _addImage: function () {
-        var extraClasses = this.options.intendedIcon.lastIndexOf('fa', 0) === 0 ? ' fa fa-lg' : ' glyphicon';
+        var extraClasses = this.options.icon.lastIndexOf('fa', 0) === 0 ? ' fa fa-lg' : ' glyphicon';
 
-        var icon = L.DomUtil.create('i', this.options.intendedIcon + extraClasses, this.link);
+        var icon = L.DomUtil.create('i', this.options.icon + extraClasses, this.link);
         icon.id = this.options.id;
     }
 });
 
-L.easyButton = function( btnIcon , btnFunction , btnTitle , btnMap , btnId) {
-  var newControl = new L.Control.EasyButtons();
+L.easyButton = function (map, callback, options) {
+    var newControl = new L.Control.EasyButtons(callback, options);
 
-  if (btnIcon) newControl.options.intendedIcon = btnIcon;
-  if (btnId) newControl.options.id = btnId;
-
-  if ( typeof btnFunction === 'function'){
-    newControl.intendedFunction = btnFunction;
-  }
-
-  if (btnTitle) newControl.options.title = btnTitle;
-
-  if ( btnMap === '' ){
-    // skip auto addition
-  } else if ( btnMap ) {
-    btnMap.addControl(newControl);
-  } else {
-    map.addControl(newControl);
-  }
-  return newControl;
+    if (map && map !== '') {
+        map.addControl(newControl);
+    }
+    return newControl;
 };
